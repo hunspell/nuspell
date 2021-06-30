d5d09a5541db53b8d06a416d71e4c8f285acd1f9
PanderMusubi <pander@users.sourceforge.net>
2021-07-01 00:11:58 +0200

    added suggest compare loop

--- tests/verify.cxx
+++ tests/verify.cxx
@@ -19,6 +19,7 @@
 #include <hunspell/hunspell.hxx>
 #include <nuspell/dictionary.hxx>
 #include <nuspell/finder.hxx>
+#include <nuspell/utils.hxx>
 
 #include <chrono>
 #include <fstream>
@@ -60,8 +61,10 @@ struct Args_t {
 	string encoding;
 	vector<string> other_dicts;
 	vector<string> files;
+	string correction;;
 	bool print_false = false;
 	bool sugs = false;
+	bool print_sug = false;
 
 	Args_t() = default;
 	Args_t(int argc, char* argv[]) { parse_args(argc, argv); }
@@ -78,7 +81,7 @@ auto Args_t::parse_args(int argc, char* argv[]) -> void
 	int c;
 	// The program can run in various modes depending on the
 	// command line options. mode is FSM state, this while loop is FSM.
-	const char* shortopts = ":d:i:fshv";
+	const char* shortopts = ":d:i:c:fsphv";
 	const struct option longopts[] = {
 	    {"version", 0, nullptr, 'v'},
 	    {"help", 0, nullptr, 'h'},
@@ -101,12 +104,24 @@ auto Args_t::parse_args(int argc, char* argv[]) -> void
 			encoding = optarg;
 
 			break;
+		case 'c':
+			if (correction.empty())
+				correction = optarg;
+			else
+				cerr << "WARNING: Ignoring additional "
+				        "suggestions TSV file "
+				     << optarg << '\n';
+
+			break;
 		case 'f':
 			print_false = true;
 
 			break;
 		case 's':
 			sugs = true;
+			break;
+		case 'p':
+			print_sug = true;
 			break;
 		case 'h':
 			if (mode == DEFAULT_MODE)
@@ -151,7 +166,7 @@ auto print_help(const string& program_name) -> void
 	auto& o = cout;
 	o << "Usage:\n"
 	     "\n";
-	o << p << " [-d dict_NAME] [-i enc] [-f] [-s] [file_name]...\n";
+	o << p << " [-d dict_NAME] [-i enc] [-c tsv] [-f] [-s] [file_name]...\n";
 	o << p << " -h|--help|-v|--version\n";
 	o << "\n"
 	     "Verification testing of Nuspell for each FILE.\n"
@@ -160,8 +175,11 @@ auto print_help(const string& program_name) -> void
 	     "  -d di_CT      use di_CT dictionary. Only one dictionary is\n"
 	     "                currently supported\n"
 	     "  -i enc        input encoding, default is active locale\n"
+	     "  -i tsv        TSV file with corrections to compare suggest\n"
+	     "                (this ignores FILE or standard input)\n"
 	     "  -f            print false negative and false positive words\n"
 	     "  -s            also test suggestions (usable only in debugger)\n"
+	     "  -p            print suggestion (only when comparing suggest)\n"
 	     "  -h, --help    print this help and exit\n"
 	     "  -v, --version print version number and exit\n"
 	     "\n";
@@ -334,6 +352,416 @@ auto normal_loop(const Args_t& args, const Dictionary& dic, Hunspell& hun,
 	out << "Speedup Rate        " << speedup << '\n';
 }
 
+auto suggest_loop(const Args_t& args, const Dictionary& dic, Hunspell& hun,
+                 istream& in, ostream& out)
+{
+	using chrono::high_resolution_clock;
+	using chrono::nanoseconds;
+
+	auto print_false = args.print_false;
+	auto word = string();
+	auto u8_buffer = string();
+	auto hun_word = string();
+	auto in_loc = in.getloc();
+	auto correction = string();
+
+	// verify spelling
+	auto total = 0;
+	auto true_pos = 0;
+	auto true_neg = 0;
+	auto false_pos = 0;
+	auto false_neg = 0;
+	auto duration_nu_tot = nanoseconds();
+	auto duration_hun_tot = nanoseconds();
+	auto duration_nu_min = nanoseconds(nanoseconds::max());
+	auto duration_hun_min = nanoseconds(nanoseconds::max());
+	auto duration_nu_max = nanoseconds();
+	auto duration_hun_max = nanoseconds();
+	auto speedup_max = 0.0;
+
+	// verify suggestions
+	auto sug_total = 0;
+	// correction is somewhere in suggestions
+	auto sug_in_nu = 0;
+	auto sug_in_hun = 0;
+	auto sug_in_both = 0;
+	// correction is first suggestion
+	auto sug_first_nu = 0;
+	auto sug_first_hun = 0;
+	auto sug_first_both = 0;
+	auto sug_same_first = 0;
+	// compared number of suggestions
+	auto sug_nu_more = 0;
+	auto sug_hun_more = 0;
+	auto sug_same_amount = 0;
+	// no suggestions
+	auto sug_nu_none = 0;
+	auto sug_hun_none = 0;
+	auto sug_both_none = 0;
+	// maximum suggestions
+	auto sug_nu_max = (size_t)0;
+	auto sug_hun_max = (size_t)0;
+	// number suggestion at or over maximum
+	auto sug_nu_at_max = 0;
+	auto sug_hun_at_max = 0;
+	auto sug_both_at_max = 0;
+
+	auto sug_duration_nu_tot = nanoseconds();
+	auto sug_duration_hun_tot = nanoseconds();
+	auto sug_duration_nu_min = nanoseconds(nanoseconds::max());
+	auto sug_duration_hun_min = nanoseconds(nanoseconds::max());
+	auto sug_duration_nu_max = nanoseconds();
+	auto sug_duration_hun_max = nanoseconds();
+	auto sug_speedup_max = 0.0;
+
+	auto sug_line = string();
+	auto sug_excluded = vector<string>();
+
+	auto uerr = U_ZERO_ERROR;
+	auto io_cnv = icu::LocalUConverterPointer(
+	    ucnv_open(args.encoding.c_str(), &uerr));
+	if (U_FAILURE(uerr))
+		throw runtime_error("Invalid io encoding");
+	auto hun_enc =
+	    nuspell::Encoding(hun.get_dict_encoding()).value_or_default();
+	auto hun_cnv =
+	    icu::LocalUConverterPointer(ucnv_open(hun_enc.c_str(), &uerr));
+	if (U_FAILURE(uerr))
+		throw runtime_error("Invalid hun encoding");
+	auto io_is_utf8 = ucnv_getType(io_cnv.getAlias()) == UCNV_UTF8;
+	auto hun_is_utf8 = ucnv_getType(hun_cnv.getAlias()) == UCNV_UTF8;
+
+	// need to take entine line here, not `in >> word`
+	while (getline(in, sug_line)) {
+		auto word_correction = vector<string>();
+		split_on_any_of(sug_line, "\t", word_correction);
+		word = word_correction[0];
+		correction = word_correction[1];
+
+		auto u8_word = string_view();
+		auto tick_a = high_resolution_clock::now();
+		if (io_is_utf8) {
+			u8_word = word;
+		}
+		else {
+			to_utf8(word, u8_buffer, io_cnv.getAlias(), uerr);
+			u8_word = u8_buffer;
+		}
+		auto res_nu = dic.spell(u8_word);
+		auto tick_b = high_resolution_clock::now();
+		if (hun_is_utf8)
+			hun_word = u8_word;
+		else
+			from_utf8(u8_word, hun_word, hun_cnv.getAlias(), uerr);
+		auto res_hun = hun.spell(hun_word);
+		auto tick_c = high_resolution_clock::now();
+		auto duration_nu = tick_b - tick_a;
+		auto duration_hun = tick_c - tick_b;
+
+		duration_nu_tot += duration_nu;
+		duration_hun_tot += duration_hun;
+		if (duration_nu < duration_nu_min)
+			duration_nu_min = duration_nu;
+		if (duration_hun < duration_hun_min)
+			duration_hun_min = duration_hun;
+		if (duration_nu > duration_nu_max)
+			duration_nu_max = duration_nu;
+		if (duration_hun > duration_hun_max)
+			duration_hun_max = duration_hun;
+
+		auto speedup = duration_hun * 1.0 / duration_nu;
+		if (speedup > speedup_max)
+			speedup_max = speedup;
+
+		if (res_hun) {
+			if (res_nu) {
+				++true_pos;
+			}
+			else {
+				++false_neg;
+				if (print_false)
+					out << "FalseNegativeWord   " << word
+					    << '\n';
+			}
+		}
+		else {
+			if (res_nu) {
+				++false_pos;
+				if (print_false)
+					out << "FalsePositiveWord   " << word
+					    << '\n';
+			}
+			else {
+				++true_neg;
+			}
+		}
+		++total;
+
+		if (res_nu || res_hun) {
+			sug_excluded.push_back(word);
+			continue;
+		}
+		// print word and expected suggestion
+		if (args.print_sug)
+			out << word << '\t' << correction << '\t';
+
+		auto sugs_nu = vector<string>();
+		tick_a = high_resolution_clock::now();
+		dic.suggest(word, sugs_nu);
+		tick_b = high_resolution_clock::now();
+		auto sugs_hun = hun.suggest(hun_word);
+		tick_c = high_resolution_clock::now();
+		auto sug_duration_nu = tick_b - tick_a;
+		auto sug_duration_hun = tick_c - tick_b;
+
+		sug_duration_nu_tot += sug_duration_nu;
+		sug_duration_hun_tot += sug_duration_hun;
+		if (sug_duration_nu < sug_duration_nu_min)
+			sug_duration_nu_min = sug_duration_nu;
+		if (sug_duration_hun < sug_duration_hun_min)
+			sug_duration_hun_min = sug_duration_hun;
+		if (sug_duration_nu > sug_duration_nu_max)
+			sug_duration_nu_max = sug_duration_nu;
+		if (sug_duration_hun > sug_duration_hun_max)
+			sug_duration_hun_max = sug_duration_hun;
+
+		auto sug_speedup = sug_duration_hun * 1.0 / sug_duration_nu;
+		if (sug_speedup > sug_speedup_max)
+			sug_speedup_max = sug_speedup;
+
+		// print number of suggestions
+		if (args.print_sug)
+			out << sug_duration_nu.count() << '\t'
+			    << sug_duration_hun.count() << '\n';
+
+		// correction is somewhere in suggestions
+		auto in_nu = false;
+		for (auto& sug : sugs_nu) {
+			if (sug == correction) {
+				++sug_in_nu;
+				in_nu = true;
+				break;
+			}
+		}
+		for (auto& sug : sugs_hun) {
+			if (sug == correction) {
+				++sug_in_hun;
+				if (in_nu)
+					++sug_in_both;
+				break;
+			}
+		}
+
+		// correction is first suggestion
+		auto first_nu = false;
+		if (!sugs_nu.empty() && sugs_nu[0] == correction) {
+			++sug_first_nu;
+			first_nu = true;
+		}
+		if (!sugs_hun.empty() && sugs_hun[0] == correction) {
+			++sug_first_hun;
+			if (first_nu)
+				++sug_first_both;
+		}
+
+		// same first suggestion, regardless of desired correction
+		if (!sugs_nu.empty() && !sugs_hun.empty() &&
+		    sugs_nu[0] == sugs_hun[0])
+			++sug_same_first;
+
+		// compared number of suggestions
+		if (sugs_nu.size() == sugs_hun.size())
+			++sug_same_amount;
+		if (sugs_nu.size() > sugs_hun.size())
+			++sug_nu_more;
+		if (sugs_hun.size() > sugs_nu.size())
+			++sug_hun_more;
+
+		// no suggestions
+		if (sugs_nu.empty()) {
+			++sug_nu_none;
+			if (sugs_hun.empty())
+				++sug_both_none;
+		}
+		if (sugs_hun.empty())
+			++sug_hun_none;
+
+		// maximum suggestions
+		if (sugs_nu.size() > sug_nu_max)
+			sug_nu_max = sugs_nu.size();
+		if (sugs_hun.size() > sug_hun_max)
+			sug_hun_max = sugs_hun.size();
+
+		// number suggestion at or over maximum
+		if (sugs_nu.size() >= 15) {
+			++sug_nu_at_max;
+			if (sugs_hun.size() >= 15)
+				++sug_both_at_max;
+		}
+		if (sugs_hun.size() >= 15)
+			++sug_hun_at_max;
+
+		++sug_total;
+	}
+
+	// prevent division by zero
+	if (total == 0) {
+		cerr << "WARNING: No input was provided\n";
+		return;
+	}
+	if (duration_nu_tot.count() == 0) {
+		cerr
+		    << "ERROR: Invalid duration of 0 nanoseconds for Nuspell\n";
+		return;
+	}
+	if (duration_hun_tot.count() == 0) {
+		cerr << "ERROR: Invalid duration of 0 nanoseconds for "
+		        "Hunspell\n";
+		return;
+	}
+
+	// counts
+	auto pos_nu = true_pos + false_pos;
+	auto pos_hun = true_pos + false_neg;
+	auto neg_nu = true_neg + false_neg;
+	auto neg_hun = true_neg + false_pos;
+
+	// rates
+	auto true_pos_rate = true_pos * 1.0 / total;
+	auto true_neg_rate = true_neg * 1.0 / total;
+	auto false_pos_rate = false_pos * 1.0 / total;
+	auto false_neg_rate = false_neg * 1.0 / total;
+
+	auto accuracy = (true_pos + true_neg) * 1.0 / total;
+	auto precision = 0.0;
+	if (true_pos + false_pos != 0)
+		precision = true_pos * 1.0 / (true_pos + false_pos);
+	auto speedup = duration_hun_tot * 1.0 / duration_nu_tot;
+
+	// report
+	out << "Total Words Spelling        " << total << '\n';
+	out << "Positives Nuspell           " << pos_nu << '\n';
+	out << "Positives Hunspell          " << pos_hun << '\n';
+	out << "Negatives Nuspell           " << neg_nu << '\n';
+	out << "Negatives Hunspell          " << neg_hun << '\n';
+	out << "True Positives              " << true_pos << '\n';
+	out << "True Negatives              " << true_neg << '\n';
+	out << "False Positives             " << false_pos << '\n';
+	out << "False Negatives             " << false_neg << '\n';
+	out << "True Positive Rate          " << true_pos_rate << '\n';
+	out << "True Negative Rate          " << true_neg_rate << '\n';
+	out << "False Positive Rate         " << false_pos_rate << '\n';
+	out << "False Negative Rate         " << false_neg_rate << '\n';
+	out << "Total Duration Nuspell      " << duration_nu_tot.count() << '\n';
+	out << "Total Duration Hunspell     " << duration_hun_tot.count() << '\n';
+	out << "Minimum Duration Nuspell    " << duration_nu_min.count() << '\n';
+	out << "Minimum Duration Hunspell   " << duration_hun_min.count() << '\n';
+	out << "Average Duration Nuspell    " << duration_nu_tot.count() / total
+	    << '\n';
+	out << "Average Duration Hunspell   " << duration_hun_tot.count() / total
+	    << '\n';
+	out << "Maximum Duration Nuspell    " << duration_nu_max.count() << '\n';
+	out << "Maximum Duration Hunspell   " << duration_hun_max.count() << '\n';
+	out << "Maximum Speedup             " << speedup_max << '\n';
+	out << "Accuracy                    " << accuracy << '\n';
+	out << "Precision                   " << precision << '\n';
+	out << "Speedup                     " << speedup << '\n';
+
+	// prevent division by zero
+	if (sug_total == 0) {
+		cerr << "WARNING: No input for suggestions was provided\n";
+		return;
+	}
+	if (sug_duration_nu_tot.count() == 0) {
+		cerr << "ERROR: Invalid duration of 0 nanoseconds for Nuspell "
+		        "suggestions\n";
+		return;
+	}
+	if (sug_duration_hun_tot.count() == 0) {
+		cerr << "ERROR: Invalid duration of 0 nanoseconds for Hunspell "
+		        "suggestions\n";
+		return;
+	}
+
+	// rates
+	auto sug_in_nu_rate = sug_in_nu * 1.0 / sug_total;
+	auto sug_in_hun_rate = sug_in_hun * 1.0 / sug_total;
+	auto sug_first_nu_rate = sug_first_nu * 1.0 / sug_total;
+	auto sug_first_hun_rate = sug_first_hun * 1.0 / sug_total;
+
+	auto sug_speedup =
+	    sug_duration_hun_tot.count() * 1.0 / sug_duration_nu_tot.count();
+
+	// suggestion report
+	out << "Total Words Suggestion                  " << sug_total << '\n';
+	out << "Correction In Suggestions Nuspell       " << sug_in_nu << '\n';
+	out << "Correction In Suggestions Hunspell      " << sug_in_hun << '\n';
+	out << "Correction In Suggestions Both          " << sug_in_both
+	    << '\n';
+
+	out << "Correction As First Suggestion Nuspell  " << sug_first_nu
+	    << '\n';
+	out << "Correction As First Suggestion Hunspell " << sug_first_hun
+	    << '\n';
+	out << "Correction As First Suggestion Both     " << sug_first_both
+	    << '\n';
+
+	out << "Nuspell More Suggestions                " << sug_nu_more
+	    << '\n';
+	out << "Hunspell More Suggestions               " << sug_hun_more
+	    << '\n';
+	out << "Same Number Of Suggestions              " << sug_in_hun << '\n';
+
+	out << "Nuspell No Suggestions                  " << sug_nu_none
+	    << '\n';
+	out << "Hunspell No Suggestions                 " << sug_hun_none
+	    << '\n';
+	out << "Both No Suggestions                     " << sug_both_none
+	    << '\n';
+
+	out << "Maximum Suggestions Nuspell             " << sug_nu_max << '\n';
+	out << "Maximum Suggestions Hunspell            " << sug_hun_max
+	    << '\n';
+
+	out << "Rate Corr. In Suggestions Nuspell       " << sug_in_nu_rate
+	    << '\n';
+	out << "Rate Corr. In Suggestions Hunspell      " << sug_in_hun_rate
+	    << '\n';
+
+	out << "Rate Corr. As First Suggestion Nuspell  " << sug_first_nu_rate
+	    << '\n';
+	out << "Rate Corr. As First Suggestion Hunspell " << sug_first_hun_rate
+	    << '\n';
+
+	out << "Total Duration Suggestions Nuspell      "
+	    << sug_duration_nu_tot.count() << '\n';
+	out << "Total Duration Suggestions Hunspell     "
+	    << sug_duration_hun_tot.count() << '\n';
+	out << "Minimum Duration Suggestions Nuspell    "
+	    << sug_duration_nu_min.count() << '\n';
+	out << "Minimum Duration Suggestions Hunspell   "
+	    << sug_duration_hun_min.count() << '\n';
+	out << "Average Duration Suggestions Nuspell    "
+	    << sug_duration_nu_tot.count() / sug_total << '\n';
+	out << "Average Duration Suggestions Hunspell   "
+	    << sug_duration_hun_tot.count() / sug_total << '\n';
+	out << "Maximum Duration Suggestions Nuspell    "
+	    << sug_duration_nu_max.count() << '\n';
+	out << "Maximum Duration Suggestions Hunspell   "
+	    << sug_duration_hun_max.count() << '\n';
+	out << "Maximum Suggestions Speedup             " << sug_speedup_max
+	    << '\n';
+	out << "Suggestions Speedup                     " << sug_speedup
+	    << '\n';
+
+	if (!sug_excluded.empty()) {
+		out << "The following words are correct and should not be "
+		       "used:\n";
+		for (auto& excl : sug_excluded)
+			out << excl << '\n';
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	// May speed up I/O. After this, don't use C printf, scanf etc.
@@ -404,6 +832,16 @@ int main(int argc, char* argv[])
 	auto hunspell_ram = get_peak_ram_usage() - peak_ram_a;
 	cout << "Nuspell peak RAM usage:  " << nuspell_ram << "kB\n"
 	     << "Hunspell peak RAM usage: " << hunspell_ram << "kB\n";
+	if (!args.correction.empty()) {
+		ifstream in(args.correction);
+		if (!in.is_open()) {
+			cerr << "Can't open " << args.correction << '\n';
+			return 1;
+		}
+		in.imbue(cin.getloc());
+		suggest_loop(args, dic, hun, in, cout);
+		return 0;
+	}
 	if (args.files.empty()) {
 		normal_loop(args, dic, hun, cin, cout);
 	}
